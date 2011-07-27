e7a5a129a74b4adceda9036e910ddf81b11cdaa3
Tanguy Cano <tanguy@fluffyspider.com>
2011-07-27 18:24:29 +1000

    Partialy parse data and time

--- src/createSequence/parser.cpp
+++ src/createSequence/parser.cpp
@@ -2,6 +2,9 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
+
+#define FRAME_PER_SEC	1	//TODO : use the class instead
 
 Parser::Parser(std::string fileName)
 {
@@ -20,13 +23,29 @@ void Parser::configure()
 
 GpsData* Parser::parse()
 {
+	GpsData * data = new GpsData();
 	std::string line;
 	std::ifstream infile (inputFile.c_str(), std::ifstream::in);
 	while (getline(infile, line, '\n'))
 	{
-		std::cout << line << std::endl;
+//FRAME_PER_SEC * 
+//		std::string frameNumber = line.substr(0,line.find("\t"));
+		std::stringstream strStream(line.substr(6,2));
+		int frameNumber;
+		
+		strStream >> frameNumber;
+		frameNumber *= FRAME_PER_SEC;
+
+		std::cout << frameNumber << std::endl;
+		frameData* f = new frameData();
+//		data.insert(frameNumber, f); TODO HERE I WAS
+		
 	}
 	
-	return 0;
+	return data;
 }
 
+int Parser::parseTime(std::string timeString)
+{
+	return 0;
+}
