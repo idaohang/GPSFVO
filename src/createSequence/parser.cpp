f89b314329730d0a75713086524f125006418794
Tanguy Cano <tanguy@fluffyspider.com>
2011-07-29 10:20:42 +1000

    Parsing speed and angle

--- src/createSequence/parser.cpp
+++ src/createSequence/parser.cpp
@@ -30,13 +30,20 @@ GpsData* Parser::parse()
 	std::ifstream infile (inputFile.c_str(), std::ifstream::in);
 	while (getline(infile, line, '\n'))
 	{
-//		line.substr(0,line.find("\t")); FIXME
-		std::stringstream strStream(line.substr(6,2));
-		int frameNumber;
-		strStream >> frameNumber;
-		frameNumber *= FRAME_PER_SEC;
+		int frameNumber = parseTime(line.substr(0,line.find("\t")));
+		
+		int start = line.find("\t", line.find("\t")+1);
+		int end = line.find("\t", start+1);
+		std::string speed = parseSpeed(line.substr(start+1,end-start-1));
+
+		start = line.find("\t", end);
+		end = line.find(" ", start+1);
+		std::string angle = line.substr(start+1,end-start-1);
+
 		
 		frameData* f = new frameData();
+		f->insert(frameData::value_type("speed", speed));
+		f->insert(frameData::value_type("angle", angle));
 		(*data)[frameNumber] = f;
 		
 	}
@@ -48,5 +55,22 @@ GpsData* Parser::parse()
 
 int Parser::parseTime(std::string timeString)
 {
-	return 0;
+	std::stringstream strStream(timeString.substr(6,2));
+	int time;
+	strStream >> time;
+	time *= FRAME_PER_SEC;
+	return time;
 }
+
+std::string Parser::parseSpeed(std::string speedString)
+{
+	std::string returned;
+	speedString[2]='.';
+/*	std::stringstream strStream(speedString.substr(0,5));
+	float speed=0.0;
+	strStream >> speed;*/
+
+	return speedString.substr(0,5);
+}
+
+
